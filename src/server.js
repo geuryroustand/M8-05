d6d3a705a43ca7ad747e849703169d8c3d05f44f
Geury Roustand <theromantic2010@hotmail.com>
2021-11-05 11:04:11 +0100

    server added

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+
+import mongoose from "mongoose";
+
+const server = express();
+
+const port = process.env.PORT;
+
+//***********MIDDLEWARES ********************** */
+
+server.use(cors());
+server.use(express.json());
+
+//************Router ****************/
+
+mongoose.connect(process.env.DATABASE);
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to mongoDB");
+
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log("server connected");
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongo ERROR", err);
+});
+
+server.on("error", (err) => {
+  console.error("Server crashed due to ", err);
+});
